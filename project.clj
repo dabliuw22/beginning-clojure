d654ebddc5c125843fae4e3339e48e49d7e09ad9
Will Leyton <wleylop22@gmail.com>
2020-10-30 15:22:40 -0500

    Add resources dir and jdbc

--- project.clj
+++ project.clj
@@ -4,9 +4,13 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/core.async "1.3.610"]]
+                 [org.clojure/core.async "1.3.610"]
+                 [org.clojure/java.jdbc "0.7.11"]
+                 [com.h2database/h2 "1.4.200"]
+                 [org.postgresql/postgresql "42.2.12"]]
   :main com.leysoft.core
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
   :java-source-paths ["src/main/java"]
+  :resource-paths ["src/main/resources"]
   :repl-options {:init-ns com.leysoft.core})
